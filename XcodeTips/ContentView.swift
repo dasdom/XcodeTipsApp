cb838c42b616db20b4be27d0f2be0227f9e6b3f2
Dominik Hauser <dom@dasdom.de>
2022-04-08 15:08:57 +0200

    Initial Commit

---
+++ XcodeTips/ContentView.swift
@@ -0,0 +1,18 @@
+//  Created by Dominik Hauser on 08.04.22.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeTips/XcodeTipsApp.swift
@@ -0,0 +1,14 @@
+//  Created by Dominik Hauser on 08.04.22.
+//  
+//
+
+import SwiftUI
+
+@main
+struct XcodeTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
